0f31b6073a1829f571ebb894a636fde0083ad63a
Stanley <angstanley68@gmail.com>
2020-12-11 02:14:08 +0700

    feat: Add anime page

---
+++ pages/Animes/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ anime }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AnimeDetail",
+    async fetch ({ store, params }) {
+      await store.dispatch('animes/fetchAnimeDetail', params.id)
+    },
+    validate ({ params }) {
+      // Must be a string
+      return /^\S+$/.test(params.id)
+    },
+    computed: {
+      anime() {
+        return this.$store.state.animes.currentAnime
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- store/animes.js
+++ store/animes.js
@@ -1,10 +1,14 @@
 export const state = () => ({
-  animes: {}
+  animes: {},
+  currentAnime: {}
 })
 
 export const mutations = {
   SET_ANIMES (state, payload) {
     state.animes = payload
+  },
+  SET_CURRENT_ANIME (state, payload)  {
+    state.currentAnime = payload
   }
 }
 
@@ -12,5 +16,10 @@ export const actions = {
   async fetchAnimes ({ commit }) {
     const { data } = await this.$axios.get('/api/v1/animes')
     commit('SET_ANIMES', data)
-  }
+  },
+  async fetchAnimeDetail ({ commit }, animeId) {
+    const { data } = await this.$axios.get(`/api/v1/animes/${animeId}`)
+    commit('SET_CURRENT_ANIME', data)
+  },
+
 }
