efe1a2ec4a54139791897eff2a300207649e5326
Stanley <angstanley68@gmail.com>
2020-12-11 02:14:20 +0700

    feat: Add anime detail page

--- pages/Animes/index.vue
+++ pages/Animes/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    {{ animes }}
+    <div v-for="anime in animesList" :key="anime.id">
+      <ul>
+        <li><nuxt-link :to="`/animes/${anime.id}`">{{ anime.name }}</nuxt-link></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -10,11 +14,21 @@ export default {
   async fetch ({ store, params }) {
     await store.dispatch('animes/fetchAnimes')
   },
+  data() {
+    return {
+    }
+  },
   computed: {
     animes () {
       return this.$store.state.animes.animes
+    },
+    animesList () {
+      return this.animes.animes.data
+    },
+    pagesList () {
+      return this.animes.animes.links
     }
-  }
+  },
 }
 </script>
 
